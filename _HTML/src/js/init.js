400b09087fd322032a694adaf5fcc25d357364db
Angelwayfarer <angelwayfarer@gmail.com>
2016-08-14 18:29:26 +0300

    WEBPACK 1

--- _HTML/src/js/init.js
+++ _HTML/src/js/init.js
@@ -1,31 +1,30 @@
 import wHTML from 'wHTML';
-
 jQuery(document).ready(function ($) {
     svg4everybody({});
 
+	/* Подключение скрипта левого меню */
     let $multiLevelMenu = $('.js-multiLevelMenu');
     if ($multiLevelMenu.length){
         require.ensure([],function(){
             let initAsideMenu = require('asideMenu');
             initAsideMenu($multiLevelMenu);
         })
-
     }
-    let $magnificSelectors  = $('.mfi-gallery');
+    /* Подключение магнифика при необходимости галлереи*/
+    let $magnificSelectors  = $('.mfi-gallery').add('.videoLink');
     if($magnificSelectors.length){
         require.ensure([],function () {
-            console.log('before');
             require('magnific-popup');
-            console.log('after');
             $('.mfi-gallery').magnificPopup({
                 delegate: 'a',
                 type: 'image',
                 gallery: {
                     enabled: true
                 }
-
             });
-        })
-
+			$('a.videoLink').magnificPopup({
+				type: 'iframe'
+			});
+		})
     }
 });
