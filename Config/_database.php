4644b57c82c9412ad0461ff915334a01d17214a3
Кучура Дмитрий Анатольевич <kuchura.d@wezom.lan>
2016-10-21 15:56:35 +0300

    fix database

--- Config/_database.php
+++ Config/_database.php
@@ -16,10 +16,11 @@ return [
              *
              * Ports and sockets may be appended to the hostname.
              */
-            'hostname' => '91.206.30.13',
+            'hostname' => '91.200.60.69',
+            'port' => '3310',
             'database' => 'sensitive_db',
             'username' => 'sensitive_db',
-            'password' => 'H4c3O5h5',
+            'password' => '3G2z5R7q',
             'persistent' => false,
         ],
         'table_prefix' => '',
